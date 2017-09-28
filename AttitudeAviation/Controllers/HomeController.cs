d6544177169167a18107f5a23310ec67ec736a45
Nolan <Nolan@NOLAN-PC>
2017-09-28 14:07:38 -0400

    Hello

---
+++ AttitudeAviation/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttitudeAviation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Welcome to Attitude Aviation!";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Wish to contact Attitude Aviation?";
+
+            return View();
+        }
+
+        public IActionResult Rent()
+        {
+            ViewData["Message"] = "Schedule Rentals Online";
+
+            return View();
+        }
+
+        public IActionResult Maintenance()
+        {
+            ViewData["Message"] = "Schedule Maintenance Online";
+
+            return View();
+        }
+
+        public IActionResult Lessons()
+        {
+            ViewData["Message"] = "Schedule Flight Lessons Online";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
